5a4f9f62ae53f127d032f0a40fdc1a821e1a6303
MutsumiMatsuda <matsuda_mutsumi@r3.dion.ne.jp>
2019-02-23 21:25:43 +0900

    fixed app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -44,16 +44,13 @@ if (windowWidth < windowSm) {
     $("#dropdown").addClass("dropdown-menu");
 }
 
-
-<script>
-    $(function(){
-        $(".btn-dell").click(function () {
-            if (confirm("本当に削除しますか？")) {
-                //そのままsubmit（削除）
-            } else {
-                //cancel
-                return false;
-            }
-        });
+$(function(){
+    $(".btn-dell").click(function () {
+        if (confirm("本当に削除しますか？")) {
+            //そのままsubmit（削除）
+        } else {
+            //cancel
+            return false;
+        }
     });
-  </script>
+});
